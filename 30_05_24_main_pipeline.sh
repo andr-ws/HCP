f9a7b0ca377304ce8fc465af43dac0fe78b8e949
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-06-04 18:12:57 +0100

    Update 30_05_24_main_pipeline.sh

--- 30_05_24_main_pipeline.sh
+++ 30_05_24_main_pipeline.sh
@@ -3,115 +3,125 @@
 # ANTs XFM's followed by c3d based conversion to FSL format
 
 # Base directory
-BASE=/Users/neuro-239/Desktop/HCP
+BASE=/Users/neuro-239/Desktop/HCP/
+T1DIR=${BASE}T1_preproc/
+T2DIR=${BASE}T2_preproc/
+B0DIR=${BASE}b0_preproc/
+XFMSDIR=${BASE}xfms/
+MNIDIR=${BASE}MNI/
 
-for DIR in ${BASE}/T1_preproc/m*
+for DIR in ${T1DIR}/T1_preproc/m*
 do
   SUB=$(basename ${DIR})
-
-  mkdir -p ${BASE}/xfms/${SUB}/coreg
-
+  MODS=("T1p" "T2p")
+  mkdir -p ${XFMSDIR}${SUB}/coreg
+  i=0
   # Process T1 and T2-weighted modalities
-  for M in T1 T2
+  for M in $T1DIR $T2DIR
   do
-    # T1p is processed T1
     fslreorient2std \
-    ${BASE}/${M}_preproc/${SUB}/${M}.nii.gz \
-    ${BASE}/${M}_preproc/${SUB}/${M}p.nii.gz
+    ${M}${SUB}/${MODS[i]}.nii.gz \
+    ${M}${SUB}/${MODS[i]}.nii.gz
 
     robustfov \
-    -i ${BASE}/${M}_preproc/${SUB}/${M}.nii.gz \
-    -r ${BASE}/${M}_preproc/${SUB}/${M}p.nii.gz
+    -i ${M}${SUB}/${MODS[i]}.nii.gz \
+    -r ${M}${SUB}/${MODS[i]}.nii.gz
 
     N4BiasFieldCorrection \
     -d 3 \
-    -i ${BASE}/${M}_preproc/${SUB}/${M}p.nii.gz \
-    -o ${BASE}/${M}_preproc/${SUB}/${M}p.nii.gz
+    -i ${M}${SUB}/${MODS[i]}.nii.gz \
+    -o ${M}${SUB}/${MODS[i]}.nii.gz
 
     # Brain extract N4
     deepbet-cli \
-    --input ${BASE}/${M}_preproc/${SUB}/${M}p.nii.gz \
-    --output ${BASE}/${M}_preproc/${SUB}/${M}p_brain.nii.gz \
-    --mask ${BASE}/${M}_preproc/${SUB}/${M}p_brain_mask.nii.gz
+    --input ${M}${SUB}/${MODS[i]}.nii.gz \
+    --output ${M}${SUB}/${MODS[i]}_brain.nii.gz \
+    --mask ${M}${SUB}/${MODS[i]}_brain_mask.nii.gz
+    
+    i=$((i+1))
   done
 
   # Coregister T1p & T2p
   antsRegistrationSyN.sh \
   -d 3 \
-  -f ${BASE}/T1_preproc/${SUB}/T1p.nii.gz \
-  -m ${BASE}/T2_preproc/${SUB}/T2p.nii.gz \
-  -o ${BASE}/xfms/${SUB}/coreg/${SUB}_T2p_coreg_T1p_ \
+  -f ${T1DIR}${SUB}/T1p.nii.gz \
+  -m ${T2DIR}${SUB}/T2p.nii.gz \
+  -o ${XFMSDIR}${SUB}/coreg/${SUB}_T2p_coreg_T1p_ \
   -t r
   
   # Move warped image into T2
-  mv ${BASE}/xfms/${SUB}/coreg/${SUB}_T2p_coreg_T1p_Warped.nii.gz \
-  ${BASE}/T2_preproc/${SUB}/
+  mv ${XFMSDIR}${SUB}/coreg/${SUB}_T2p_coreg_T1p_Warped.nii.gz \
+  ${T2DIR}${SUB}/
 
   # Compute structural to standard warps (MNI versions 05mm and 2mm) for probtrackx2 and MIST (respectively)
   for V in 05mm 2mm
   do
-    mkdir -p ${BASE}/xfms/${SUB}/norm/ANTs \
-    ${BASE}/xfms/${SUB}/norm/FSL
+    mkdir -p ${XFMSDIR}${SUB}/norm/ANTs ${XFMSDIR}${SUB}/norm/FSL
 
     antsRegistrationSyN.sh \
     -d 3 \
-    -f ${BASE}/MNI/MNI152_T1_${V}_brain.nii.gz \
-    -m ${BASE}/T1_preproc/${SUB}/T1p_brain.nii.gz \
-    -x ${BASE}/MNI/MNI152_T1_${V}_brain_mask.nii.gz,${BASE}/T1_preproc/${SUB}/T1p_brain_mask.nii.gz \
-    -o ${BASE}/xfms/${SUB}/norm/ANTs/${SUB}_${V}_
+    -f ${MNIDIR}MNI152_T1_${V}_brain.nii.gz \
+    -m ${T1DIR}${SUB}/T1p_brain.nii.gz \
+    -x ${MNIDIR}MNI152_T1_${V}_brain_mask.nii.gz,${T1DIR}${SUB}/T1p_brain_mask.nii.gz \
+    -o ${XFMSDIR}${SUB}/norm/ANTs/${SUB}_${V}_
+    
     c3d_affine_tool \
-    -ref ${BASE}/MNI/MNI152_T1_${V}_brain.nii.gz \
-    -src ${BASE}/t1_preproc/${SUB}/T1p_brain.nii.gz \
-    -itk ${BASE}/xfms/${SUB}/norm/ANTs/${SUB}_${V}_0GenericAffine.mat \
+    -ref ${MNIDIR}MNI152_T1_${V}_brain.nii.gz \
+    -src ${T1DIR}${SUB}/T1p_brain.nii.gz \
+    -itk ${XFMSDIR}${SUB}/norm/ANTs/${SUB}_${V}_0GenericAffine.mat \
     -ras2fsl \
-    -o ${BASE}/xfms/${SUB}/norm/FSL/T1_2_${V}_FSL_affine.mat
+    -o ${XFMSDIR}${SUB}/norm/FSL/${SUB}_T1_2_${V}_FSL_affine.mat
+    
     c3d \
-    -mcs ${BASE}/xfms/${SUB}/norm/ANTs/${SUB}_${V}_1Warp.nii.gz \
+    -mcs ${XFMSDIR}${SUB}/norm/ANTs/${SUB}_${V}_1Warp.nii.gz \
     -oo \
-    ${BASE}/xfms/${SUB}/norm/FSL/t_wx.nii.gz \
-    ${BASE}/xfms/${SUB}/norm/FSL/t_wy.nii.gz \
-    ${BASE}/xfms/${SUB}/norm/FSL/t_wz.nii.gz
+    ${XFMSDIR}${SUB}/norm/FSL/t_wx.nii.gz \
+    ${XFMSDIR}${SUB}/norm/FSL/t_wy.nii.gz \
+    ${XFMSDIR}${SUB}/norm/FSL/t_wz.nii.gz
+    
     fslmaths \
-    ${BASE}/xfms/${SUB}/norm/FSL/t_wy.nii.gz \
+    ${XFMSDIR}${SUB}/norm/FSL/t_wy.nii.gz \
     -mul -1 \
-    ${BASE}/xfms/${SUB}/norm/FSL/t_wiy.nii.gz
+    ${XFMSDIR}${SUB}/norm/FSL/t_wiy.nii.gz
+   
     fslmerge \
     -t \
-    ${BASE}/xfms/${SUB}/norm/FSL/T1_2_${V}_FSL_nonlinear.nii.gz \
-    ${BASE}/xfms/${SUB}/norm/FSL/t_wx.nii.gz \
-    ${BASE}/xfms/${SUB}/norm/FSL/t_wiy.nii.gz \
-    ${BASE}/xfms/${SUB}/norm/FSL/t_wz.nii.gz
+    ${XFMSDIR}${SUB}/norm/FSL/${SUB}_T1_2_${V}_FSL_nonlinear.nii.gz \
+    ${XFMSDIR}${SUB}/norm/FSL/t_wx.nii.gz \
+    ${XFMSDIR}${SUB}/norm/FSL/t_wiy.nii.gz \
+    ${XFMSDIR}${SUB}/norm/FSL/t_wz.nii.gz
 
     # Remove temporary xfms
-    rm ${BASE}/xfms/${SUB}/norm/FSL/t_*.nii.gz
+    rm ${XFMSDIR}${SUB}/norm/FSL/t_*.nii.gz
 
     # Concatenate affine and warp xfms
     convertwarp \
-    --ref=${BASE}/MNI/MNI152_T1_${V}_brain.nii.gz \
-    --premat=${BASE}/xfms/${SUB}/norm/FSL/T1_2_${V}_FSL_affine.mat \
-    --warp1=${BASE}/xfms/${SUB}/norm/FSL/T1_2_${V}_FSL_nonlinear.nii.gz \
-    --out=${BASE}/xfms/${SUB}/norm/FSL/T1_2_${V}_FSL_warp.nii.gz
+    --ref=${MNIDIR}MNI152_T1_${V}_brain.nii.gz \
+    --premat=${XFMSDIR}${SUB}/norm/FSL/${SUB}_T1_2_${V}_FSL_affine.mat \
+    --warp1=${XFMSDIR}${SUB}/norm/FSL/${SUB}_T1_2_${V}_FSL_nonlinear.nii.gz \
+    --out=${XFMSDIR}${SUB}/norm/FSL/${SUB}_T1_2_${V}_FSL_warp.nii.gz
   done
 
 # Compute a diffusion to structural mapping
 
   epi_reg \
-  --epi=${BASE}/b0_preproc/${SUB}/b0.nii.gz \
-  --t1=${BASE}/T1_preproc/${SUB}/T1p.nii.gz \
-  --t1brain=${BASE}/T1_preproc/${SUB}/T1p_brain.nii.gz \
-  --out=${BASE}/xfms/${SUB}/coreg/d_2_T1
+  --epi=${B0DIR}${SUB}/b0.nii.gz \
+  --t1=${T1DIR}${SUB}/T1p.nii.gz \
+  --t1brain=${T1DIR}${SUB}/T1p_brain.nii.gz \
+  --out=${XFMSDIR}${SUB}/coreg/${SUB}_d_2_T1
 
     # Remove temporary xfms
-  rm ${BASE}/xfms/${SUB}/coreg/*fast*
+  rm ${XFMSDIR}${SUB}/coreg/*fast*
 
 # Compute a diffusion to standard warp (and inverse; only required for 05mm as 2mm is shape analysis)
   convertwarp \
-  --ref=${BASE}/MNI/MNI152_T1_05mm_brain.nii.gz \
-  --premat=${BASE}/xfms/${SUB}/coreg/d_2_T1.mat \
-  --warp1=${BASE}/xfms/${SUB}/norm/FSL/T1_2_05mm_FSL_warp.nii.gz \
-  --out=${BASE}/xfms/${SUB}/norm/FSL/d_2_05mm_FSL_warp.nii.gz
+  --ref=${MNIDIR}MNI152_T1_05mm_brain.nii.gz \
+  --premat=${XFMSDIR}${SUB}/coreg/${SUB}_d_2_T1.mat \
+  --warp1=${XFMSDIR}${SUB}/norm/FSL/${SUB}_T1_2_05mm_FSL_warp.nii.gz \
+  --out=${XFMSDIR}${SUB}/norm/FSL/${SUB}_d_2_05mm_FSL_warp.nii.gz
+  
   invwarp \
-  --ref=${BASE}/b0_preproc/${SUB}/b0.nii.gz \
-  --warp=${BASE}/xfms/${SUB}/norm/FSL/d_2_05mm_FSL_warp.nii.gz \
-  --out=${BASE}/xfms/${SUB}/norm/FSL/05mm_2_d_FSL_warp.nii.gz
+  --ref=${B0DIR}${SUB}/b0.nii.gz \
+  --warp=${XFMSDIR}${SUB}/norm/FSL/${SUB}_d_2_05mm_FSL_warp.nii.gz \
+  --out=${XFMSDIR}${SUB}/norm/FSL/${SUB}_05mm_2_d_FSL_warp.nii.gz
 done
