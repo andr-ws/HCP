69acf0e35692c061eca361ddd8f3c426c49ecd82
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-06-05 15:32:16 +0100

    Update HCP_MIST_PTX.sh

--- HCP_MIST_PTX.sh
+++ HCP_MIST_PTX.sh
@@ -4,20 +4,23 @@
 # Will likely use STN, Striatum and Pallidum segmentations
 
 BASE=/Users/neuro-239/Desktop/HCP/
-MISTDIR=${BASE}/MIST/
-T1DIR=${BASE}/T1_preproc/
-XFMSDIR=${BASE}/xfms/
+MISTDIR=${BASE}MIST/
+T1DIR=${BASE}T1_preproc/
+XFMSDIR=${BASE}xfms/
 mkdir -p ${MISTDIR}model
 touch ${MISTDIR}model/mist_subjects
 
 # FSL affine and warp xfms for T1 to 2mm MNI space
-A="_T1_2_2mm_FSL_affine.mat"
-W="_T1_2_2mm_FSL_warp.nii.gz"
+A="T1_2_2mm_FSL_affine.mat"
+W="T1_2_2mm_FSL_warp.nii.gz"
 
 for DIR in ${T1DIR}*
 do
+  SUB=$(basename ${DIR})
+  ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${A} ${XFMSDIR}${SUB}/norm/FSL/${A}
+  ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${W} ${XFMSDIR}${SUB}/norm/FSL/${W}
   echo ${DIR} >> ${MISTDIR}model/mist_subjects
-  echo -e "T1","T1","T1p.nii.gz",1.0\n"T2","T2","T2p.nii.gz",1.0\n"alternate_affine","${XFMSDIR}${SUB}/norm/FSL/${SUB}${A}"\n"alternate_warp","${XFMSDIR}${SUB}/norm/FSL/${SUB}${W}" >> ${MISTDIR}/mist_filenames
+  echo -e '"T1","T1","T1p.nii.gz",1.0\n"T2","T2","T2p.nii.gz",1.0\n"alternate_affine","norm/FSL/T1_2_2mm_FSL_affine.mat"\n"alternate_warp","norm/FSL/T1_2_2mm_FSL_warp.nii.gz"' >> ${MISTDIR}/mist_filenames
 done
 
 cd ${T1DIR}
