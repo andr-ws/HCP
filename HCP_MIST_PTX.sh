d4f31d026aa2ad295fde6e074df9202cdfa5e7c0
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-06-05 16:36:34 +0100

    Update HCP_MIST_PTX.sh

--- HCP_MIST_PTX.sh
+++ HCP_MIST_PTX.sh
@@ -26,8 +26,9 @@ do
   ln -s ${T1DIR}${SUB}/T1p.nii.gz ${TMPDIR}${SUB}/
   ln -s ${T2DIR}${SUB}/T2p.nii.gz ${TMPDIR}${SUB}/
   echo ${TMPDIR}${SUB} >> ${MISTDIR}mist_subjects
-  echo -e '"T1","T1","T1p.nii.gz",1.0\n"T2","T2","T2p.nii.gz",1.0\n"alternate_affine","T1_2_2mm_FSL_affine.mat"\n"alternate_warp","T1_2_2mm_FSL_warp.nii.gz"' >> ${MISTDIR}/mist_filenames
 done
+
+echo -e '"T1","T1","T1p.nii.gz",1.0\n"T2","T2","T2p.nii.gz",1.0\n"alternate_affine","T1_2_2mm_FSL_affine.mat"\n"alternate_warp","T1_2_2mm_FSL_warp.nii.gz"' >> ${MISTDIR}/mist_filenames
 
 cd ${MISTDIR}
 mist_1_train
