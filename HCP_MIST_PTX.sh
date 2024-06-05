91ef6c625d686745426c0a112ffcb01d7aba4181
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-06-05 16:14:34 +0100

    Update HCP_MIST_PTX.sh

--- HCP_MIST_PTX.sh
+++ HCP_MIST_PTX.sh
@@ -8,19 +8,25 @@ MISTDIR=${BASE}MIST/
 T1DIR=${BASE}T1_preproc/
 XFMSDIR=${BASE}xfms/
 mkdir -p ${MISTDIR}
-touch ${MISTDIR}model/mist_subjects
+touch ${MISTDIR}/mist_subjects
 
 # FSL affine and warp xfms for T1 to 2mm MNI space
 A="T1_2_2mm_FSL_affine.mat"
 W="T1_2_2mm_FSL_warp.nii.gz"
 
-for DIR in ${T1DIR}*
+# Create a temporary directory to run MIST in
+TMPDIR=${MISTDIR}tmp/
+
+for DIR in ${XFMSDIR}*
 do
   SUB=$(basename ${DIR})
-  ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${A} ${XFMSDIR}${SUB}/norm/FSL/${A}
-  ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${W} ${XFMSDIR}${SUB}/norm/FSL/${W}
-  echo ${DIR} >> ${MISTDIR}mist_subjects
-  echo -e '"T1","T1","T1p.nii.gz",1.0\n"T2","T2","T2p.nii.gz",1.0\n"alternate_affine","norm/FSL/T1_2_2mm_FSL_affine.mat"\n"alternate_warp","norm/FSL/T1_2_2mm_FSL_warp.nii.gz"' >> ${MISTDIR}/mist_filenames
+  mkdir -p ${TMPDIR}${SUB}
+  ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${A} ${TMPDIR}${SUB}/${A}
+  ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${W} ${TMPDIR}${SUB}/${W}
+  ln -s ${T1DIR}${SUB}/T1p.nii.gz ${TMPDIR}${SUB}/
+  ln -s ${T2DIR}${SUB}/T2p.nii.gz ${TMPDIR}${SUB}/
+  echo ${TMPDIR}${SUB} >> ${MISTDIR}mist_subjects
+  echo -e '"T1","T1","T1p.nii.gz",1.0\n"T2","T2","T2p.nii.gz",1.0\n"alternate_affine","T1_2_2mm_FSL_affine.mat"\n"alternate_warp","T1_2_2mm_FSL_warp.nii.gz"' >> ${MISTDIR}/mist_filenames
 done
 
 cd ${MISTDIR}
@@ -28,23 +34,7 @@ mist_1_train
 mist_2_fit
 cd ${BASE}
 
-
-
-
-# Step 6 - Write out MIST filenames
-# Subset for mist_training_subjects is all patients.
-
-touch ${MODDIR}${OUTDIR}mist_subjects
-echo ${MODDIR}${OUTDIR}${SUB} >> ${OUTDIR}/mist_subjects
-
-echo -e '"T1","T1","T1_mist_input.nii.gz",1.0\n"FLAIR","T2","T2_mist_input.nii.gz",1.5\n"alternate_affine","T1_2_std_2mm_affine_FSL.mat"\n"alternate_warp","T1_2_std_2mm_warp_FSL.nii.gz"' >> ${MODDIR}flair/mist_filenames
-echo -e '"T1","T1","T1_mist_input.nii.gz",1.0\n"T2","T2","T2_mist_input.nii.gz",1\n"alternate_affine","T1_2_std_2mm_affine_FSL.mat"\n"alternate_warp","T1_2_std_2mm_warp_FSL.nii.gz"' >> ${MODDIR}t2/mist_filenames
-
-for DIR in flair t2; do
-cd ${MODDIR}${DIR}
-mist_1_train
-mist_2_fit
-done
+# 
 
 # RUN THIS ONCE XFMS DONE SO CAN ASSESS OUTPUT
 
