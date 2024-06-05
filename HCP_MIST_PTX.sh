bcd7bca2978f78bdf5a972e84fe144b566537b38
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-06-05 15:36:55 +0100

    Update HCP_MIST_PTX.sh

--- HCP_MIST_PTX.sh
+++ HCP_MIST_PTX.sh
@@ -7,7 +7,7 @@ BASE=/Users/neuro-239/Desktop/HCP/
 MISTDIR=${BASE}MIST/
 T1DIR=${BASE}T1_preproc/
 XFMSDIR=${BASE}xfms/
-mkdir -p ${MISTDIR}model
+mkdir -p ${MISTDIR}
 touch ${MISTDIR}model/mist_subjects
 
 # FSL affine and warp xfms for T1 to 2mm MNI space
@@ -19,14 +19,32 @@ do
   SUB=$(basename ${DIR})
   ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${A} ${XFMSDIR}${SUB}/norm/FSL/${A}
   ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${W} ${XFMSDIR}${SUB}/norm/FSL/${W}
-  echo ${DIR} >> ${MISTDIR}model/mist_subjects
+  echo ${DIR} >> ${MISTDIR}mist_subjects
   echo -e '"T1","T1","T1p.nii.gz",1.0\n"T2","T2","T2p.nii.gz",1.0\n"alternate_affine","norm/FSL/T1_2_2mm_FSL_affine.mat"\n"alternate_warp","norm/FSL/T1_2_2mm_FSL_warp.nii.gz"' >> ${MISTDIR}/mist_filenames
 done
 
-cd ${T1DIR}
+cd ${MISTDIR}
 mist_1_train
 mist_2_fit
 cd ${BASE}
+
+
+
+
+# Step 6 - Write out MIST filenames
+# Subset for mist_training_subjects is all patients.
+
+touch ${MODDIR}${OUTDIR}mist_subjects
+echo ${MODDIR}${OUTDIR}${SUB} >> ${OUTDIR}/mist_subjects
+
+echo -e '"T1","T1","T1_mist_input.nii.gz",1.0\n"FLAIR","T2","T2_mist_input.nii.gz",1.5\n"alternate_affine","T1_2_std_2mm_affine_FSL.mat"\n"alternate_warp","T1_2_std_2mm_warp_FSL.nii.gz"' >> ${MODDIR}flair/mist_filenames
+echo -e '"T1","T1","T1_mist_input.nii.gz",1.0\n"T2","T2","T2_mist_input.nii.gz",1\n"alternate_affine","T1_2_std_2mm_affine_FSL.mat"\n"alternate_warp","T1_2_std_2mm_warp_FSL.nii.gz"' >> ${MODDIR}t2/mist_filenames
+
+for DIR in flair t2; do
+cd ${MODDIR}${DIR}
+mist_1_train
+mist_2_fit
+done
 
 # RUN THIS ONCE XFMS DONE SO CAN ASSESS OUTPUT
 
