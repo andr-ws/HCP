abeb3f12f2fc9c54691aa096511ffba48a6ef8bf
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-06-22 10:15:29 +0100

    Update masks.sh

--- masks.sh
+++ masks.sh
@@ -2,8 +2,11 @@
 
 # Code to create masks for tractography
 
-BASE=/ /
-FSDIR=${BASE}FS
+BASE=/Users/neuro-239/Desktop/HCP/
+T1DIR=${BASE}T1_preproc/
+FSDIR=${BASE}/FS/
+MNIDIR=${BASE}/MNI/
+XFMSDIR=${BASE}/xfms/
 
 for DIR in ${FSDIR}/s*
 do
@@ -11,18 +14,42 @@ do
 
   for IMG in aseg filled
   do
-	  mkdir ${DIR}/masks
-	  mri_convert ${DIR}/mri/${IMG}.mgz ${FSDIR}/masks/${IMG}.nii.gz
+  	mkdir ${DIR}/masks
+   	mri_convert ${DIR}/mri/${IMG}.mgz ${FSDIR}/masks/${IMG}.nii.gz
+
+    	# Coreg to T1p prior to xfm (MAYBE SHOULD COREG THE PROCESSED FS T1 
+     	# to T1p first? then apply the same xfms?
+
+     	antsRegistrationSyN \
+      	-d 3 \
+       	-f ${T1DIR}${SUB}/${SUB}_T1p.nii.gz \
+	-m ${FSDIR}/masks/${IMG}.nii.gz \
+ 	-t a \ 
+ 	-o ${FSDIR}/masks/${IMG}_coreg.nii.gz \
+
+	# Current issue is that when applying the XFM to the FS masks, does not
+ 	# align. This may be because FS used the unprocessed T1 as input, whereas
+  	# the xfms are using the T1p
+   
+     	antsApplyTransforms \
+     	-d 3 \
+      	-i ${FSDIR}masks/${IMG}.nii.gz \
+       	-r ${MNIDIR}MNI152_T1_05mm.nii.gz \
+	-t ${XFMDIR}${SUB}/norm/ANTs/${SUB}_05mm_1Warp.nii.gz \
+ 	-n NearestNeighbor \
+ 	-o ${FSDIR}masks/${IMG}_05mm.nii.gz
+   		
   done
 
   # Ventricles
   for idx in 4 14 43
   do
-	  fslmaths ${FSDIR}/masks/aseg.nii.gz \
+	  fslmaths ${FSDIR}/masks/aseg_05mm.nii.gz \
 	  -thr ${idx} -uthr ${idx} \
-	  ${FSDIR}/masks/aseg_${idx}.nii.gz
+	  ${FSDIR}/masks/aseg_05mm_${idx}.nii.gz
   done
 
+  # Combine ventricle masks
   fslmaths ${FSDIR}/masks/aseg_4.nii.gz \
           -add ${FSDIR}/masks/aseg_14.nii.gz \
           -add ${FSDIR}/masks/aseg_43.nii.gz \
@@ -32,7 +59,8 @@ do
   fslmaths ${FSDIR}/masks/filled.nii.gz \
           -thr 127 -uthr 127 \
           fslmaths ${FSDIR}/masks/rh.nii.gz
-# lh mask
-fslmaths ${FSDIR}/masks/filled.nii.gz \
+  # lh mask
+  fslmaths ${FSDIR}/masks/filled.nii.gz \
           -thr 255 -uthr 255 fslmaths \
           ${FSDIR}/masks/rh.nii.gz
+done
