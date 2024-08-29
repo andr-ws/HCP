ecb6b15d564729224dbaacf4896274e333d242ee
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-08-29 12:13:26 +0100

    Update mist.sh

--- mist.sh
+++ mist.sh
@@ -1,41 +1,74 @@
 #! /bin/bash
 
-# Code to run MIST segmentation on HCP data
-# Will likely use STN, Striatum and Pallidum segmentations
+# Shell script for MIST segementation using T1w and T2w images
 
 base=./Users/neuro-239/Desktop/HCP/
-
-
-
-
-MISTDIR=${BASE}MIST/
-T1DIR=${BASE}T1_preproc/
-XFMSDIR=${BASE}xfms/
-mkdir -p ${MISTDIR}
-touch ${MISTDIR}/mist_subjects
-
-# FSL affine and warp xfms for T1 to 2mm MNI space
-A="T1_2_2mm_FSL_affine.mat"
-W="T1_2_2mm_FSL_warp.nii.gz"
+mni=${global}/MNI
 
 # Create a temporary directory to run MIST in
-TMPDIR=${MISTDIR}tmp/
+tmp_mist-dir=${derivatives}/tmp_mist-dir
+mkdir -p ${tmp_mist-dir}
+
+# Create a .txt file containing subject names
+touch ${tmp_mist-dir}/mist_subjects
+
+# Define shortcut output names for FSL affine and affwarp xfms.
+aff="T1_2_2mm_FSL_affine.mat"
+warp="T1_2_2mm_FSL_warp.nii.gz"
 
 # Iterate over each xfm directory
 for dir in ${derivatives}/data/sub-*; do
   sub=$(basename ${dir})
+
+    # T1w-MNI (MNI152-2mm)
+    antsRegistrationSyN.sh \
+    -d 3 \
+    -f ${mni}/MNI152_T1_2mm_brain.nii.gz \
+    -m ${dir}/anat/${sub}_desc-bias_cor_T1w_brain.nii.gz \
+    -x ${mni}/MNI152_T1_2mm_brain_mask.nii.gz,${dir}/anat/${sub}_desc-bias_cor_T1w_brain_mask.nii.gz \
+    -o ${dir}/xfm/${sub}_T1w-mni_2mm_
+
+    # Convert ANTs affine xfm to FSL
+    c3d_affine_tool \
+    -ref ${mni}/MNI152_T1_2mm_brain.nii.gz \
+    -src ${dir}/anat/${sub}_desc-bias_cor_T1w_brain.nii.gz \
+    -itk ${dir}/xfm/norm/ANTs/${sub}_T1w-mni_2mm_0GenericAffine.mat \
+    -ras2fsl \
+    -o ${dir}/xfm/norm/FSL/${sub}_T1w-mni_2mm_affine.mat
+
+    # Convert ANTs warp xfm to FSL
+    wb_command \
+    -convert-warpfield -from-itk \
+    ${dir}/xfm/norm/ANTs/${sub}_T1w-mni_2mm_1Warp.nii.gz \
+    -to-fnirt \
+    ${dir}/xfm/norm/FSL/${sub}_T1w-mni_2mm_warp.nii.gz \
+    ${mni}/MNI152_T1_2mm_brain.nii.gz
+
+    # Concatenate FSL-converted affine and warpfields
+    convertwarp \
+    --ref=${mni}/MNI152_T1_2mm_brain.nii.gz \
+    --premat=${dir}/xfm/norm/FSL/${sub}_T1w-mni_2mm_affine.mat \
+    --warp1=${dir}/xfm/norm/FSL/${sub}_T1w-mni_2mm_warp.nii.gz \
+    --out=${dir}/xfm/norm/FSL/${sub}_T1w-mni_2mm_affwarp.nii.gz
+
+    # Create temporary participant direcs and populate files
+    mkdir -p ${tmp_mist-dir}/${sub}
+
+    # Link FSL affine and warps
+    ln -s ${dir}/xfm/norm/FSL/${sub}_T1w-mni_2mm_affine.mat ${tmp_mist-dir}/${sub}/${aff}
+    ln -s ${dir}/xfm/norm/FSL/${sub}_T1w-mni_2mm_affwarp.nii.gz ${tmp_mist-dir}/${sub}/${warp}
+
+    # Link bias-corrected T1w/T1w_brain images
+    ln -s ${dir}/anat/${sub}_desc-bias_cor_T1w.nii.gz ${tmp_mist-dir}/${sub}/T1.nii.gz
+    ln -s ${dir}/anat/${sub}_desc-bias_cor_T1w_brain.nii.gz ${tmp_mist-dir}/${SUB}/T1_brain.nii.gz
   
-  mkdir -p ${TMPDIR}${SUB}
-  ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${A} ${TMPDIR}${SUB}/${A}
-  ln -s ${XFMSDIR}${SUB}/norm/FSL/${SUB}_${W} ${TMPDIR}${SUB}/${W}
-  ln -s ${T1DIR}${SUB}/${SUB}_T1p.nii.gz ${TMPDIR}${SUB}/T1.nii.gz
-  ln -s ${T1DIR}${SUB}/${SUB}_T1p_brain.nii.gz ${TMPDIR}${SUB}/T1_brain.nii.gz
+  
   ln -s ${T2DIR}${SUB}/${SUB}_T2p_coreg_T1p_Warped.nii.gz ${TMPDIR}${SUB}/T2.nii.gz
   echo ${TMPDIR}${SUB} >> ${MISTDIR}mist_subjects
 done
 
-# If MIST works, then ammend for using the brain extracted...
-echo -e '"T1","T1","T1.nii.gz",1.0\n"T2","T2","T2.nii.gz",1.0\n"alternate_affine","T1_2_2mm_FSL_affine.mat"\n"alternate_warp","T1_2_2mm_FSL_warp.nii.gz"' >> ${MISTDIR}/mist_filenames
+# Have supplied the T1_brain here but unsure if this may work?
+echo -e '"T1","T1","T1_brain.nii.gz",1.0\n"T2","T2","T2.nii.gz",1.0\n"alternate_affine","T1_2_2mm_FSL_affine.mat"\n"alternate_warp","T1_2_2mm_FSL_warp.nii.gz"' >> ${MISTDIR}/mist_filenames
 
 cd ${MISTDIR}
 mist_1_train
