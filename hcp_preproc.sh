b11522249ddb4b70b7521b22e6f789725843f326
andr-ws <108693884+andr-ws@users.noreply.github.com>
2024-08-29 11:34:05 +0100

    Update hcp_preproc.sh

--- hcp_preproc.sh
+++ hcp_preproc.sh
@@ -35,23 +35,8 @@ sourcedata=${base}/sourcedata
 rawdata=${base}/rawdata
 derivatives=${base}/derivatives
 
-for dir in ${sourcedata}/*; do
-
-sub=$(basename $dir)
-
-mkdir $dir/dwi/
-
-mv $dir/diff/preproc/bvals.txt $dir/dwi/${sub}_bvals.txt
-mv $dir/diff/preproc/bvecs_fsl_moco_norm.txt $dir/dwi/${sub}_bvecs_fsl_moco_norm.txt
-mv $dir/diff/preproc/bvecs_moco_norm.txt $dir/dwi/${sub}_bvecs_moco_norm.txt
-mv $dir/diff/preproc/mri/diff_preproc.nii.gz $dir/dwi/${sub}_dwi_eddy.nii.gz
-
-mv $dir/diff/raw/bvals.txt $dir/dwi/${sub}_bvals.txt
-mv $dir/diff/raw/bvecs.txt $dir/dwi/${sub}_bvecs.txt
-mv $dir/diff/raw/bvecs_fsl.txt $dir/dwi/${sub}_bvecs_fsl.txt
-mv $dir/diff/raw/mri/diff.nii.gz $dir/dwi/${sub}_dwi.nii.gz
-
-done
+export FREESURFER_HOME=/Applications/freesurfer/dev
+source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
 # Reorient eddy_dwi data and create b0 files
 
@@ -64,34 +49,34 @@ done
 #				  └── reoriented/
 #				  └── eddy_files
 
-for sub in sub-1001 sub-1002 sub-1003 sub-1005 sub-1006 sub-1007 sub-1008 sub-1009 sub-1010 sub-1011; do
+for dir in ${derivatives}/data/sub-*; do
 
-mkdir -p ${derivatives}/data/${sub}/dwi/reoriented/
+  sub=$(basename ${dir})
+  
+  # Reorient eddy corrected dwi
+  mrconvert \
+  "${derivatives}/data/${sub}/dwi/${sub}_dwi_eddy.nii.gz" \
+  -fslgrad "${derivatives}/data/${sub}/dwi/${sub}_bvecs_fsl_moco_norm.txt" \
+  "${raw}/${sub}/dwi/${sub}_bvals.txt" \
+  -strides 1,2,3 \
+  "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.nii.gz" \
+  -export_grad_fsl \
+  "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.bvec" \
+  "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.bval" \
+  -force
 
-# Reorient eddy corrected dwi appropriately?
-mrconvert \
-"${derivatives}/data/${sub}/dwi/${sub}_dwi_eddy.nii.gz" \
--fslgrad "${derivatives}/data/${sub}/dwi/${sub}_bvecs_fsl_moco_norm.txt" \
-"${raw}/${sub}/dwi/${sub}_bvals.txt" \
--strides 1,2,3 \
-"${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.nii.gz" \
--export_grad_fsl \
-"${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.bvec" \
-"${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.bval" \
--force
+  # Obtain b0
+  dwiextract \
+  "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.nii.gz" - -bzero -fslgrad \
+  "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.bvec" \
+  "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.bval" | \
+  mrmath - mean "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy_b0.nii.gz" -axis 3
 
-# Obtain b0
-dwiextract \
-"${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.nii.gz" - -bzero -fslgrad \
-"${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.bvec" \
-"${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy.bval" | \
-mrmath - mean "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy_b0.nii.gz" -axis 3
-
-# Obtain brain extracted and masked b0
-mri_synthstrip \
--i "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy_b0.nii.gz" \
--o "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy_b0_brain.nii.gz" \
--m ".${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy_b0_brain_mask.nii.gz"
+  # Obtain brain extracted and masked b0
+  mri_synthstrip \
+  -i "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy_b0.nii.gz" \
+  -o "${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy_b0_brain.nii.gz" \
+  -m ".${derivatives}/data/${sub}/dwi/reoriented/${sub}_dwi_eddy_b0_brain_mask.nii.gz"
 
 done
 
